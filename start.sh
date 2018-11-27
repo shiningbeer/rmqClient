a9e89504cda40d3a4377a5875ff8e042001137dd
Your Name <you@example.com>
2018-11-27 09:48:41 +0800

    1

---
+++ start.sh
@@ -0,0 +1,16 @@
+cd src
+screennode=$'cidr'
+screen -S $screennode -X quit
+screen -dmS $screennode
+sleep 2s
+cmd=$"sudo python ./cidr_worker.py"
+screen -S $screennode -X stuff "$cmd"
+screen -S $screennode -X stuff $'\n' 
+
+screennode=$'plugin'
+screen -S $screennode -X quit
+screen -dmS $screennode
+sleep 2s
+cmd=$"sudo python ./plugin_worker.py"
+screen -S $screennode -X stuff "$cmd"
+screen -S $screennode -X stuff $'\n' 
